ac2efe87647928ece22eac6afa458a822db1083e
mhussain23 <KFZee.MR@gmail.com>
2023-04-10 13:16:56 +0530

    testing master committing with error

--- src/main/java/HelloWord.java
+++ src/main/java/HelloWord.java
@@ -1,6 +1,6 @@
  public class HelloWord {
 
-     public static void main(String[] args) {
+     public static void main(String[] args)s {
 
          System.out.println("This is my first GitHub commit integrating with Jenkins");
      }
